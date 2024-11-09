7629c94542d05bbb69cf231f94685efbf263455b
pedro <pedrosilvagomes2502@outlook.com>
2024-11-09 18:20:34 -0300

    [Implementação] Criação de função Login e Cadastro

--- login/views.py
+++ login/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.messages import constants
+from django.views.decorators.csrf import csrf_exempt
 
 def login(request):
     status = request.GET.get('status', None)
@@ -11,43 +12,49 @@ def cadastro(request):
     status = request.GET.get('status', None)
     return render(request, 'cadastro.html', {'status': status})
 
+@csrf_exempt
 def validar_login(request):
     pes_email = request.POST.get('email')
-    pes_senha = request.POST.get('senha')
+    pes_senha = request.POST.get('password')
     
-    Usuario = usuario.objects.filter(email=email, senha=senha)
-    
-    if not Usuario.exists():
+    usuario = auth.authenticate(request, username=pes_email, password=pes_senha)
+    print(usuario)
+    if usuario is None:
         messages.add_message(request, constants.WARNING, 'Email ou senha inválidos')
         return redirect('/auth/login')
-    else:
-        request.session['logado'] = True
-        return redirect('/plataforma/home')
     
-
-def validar_cadastro(request):
-    pes_nome = request.POST.get('nome')
-    pes_sobrenome = request.POST.get('sobrenome')
-    pes_email = request.POST.get('email')
-    pes_senha = request.POST.get('senha')
-    emp_nome = request.POST.get('emp_nome')
-    emp_nova = request.POST.get('emp_nova')
+    auth.login(request, usuario)
+    return redirect('/plataforma/home')
     
-    if len(pes_nome.strip()) == 0 or len(pes_email.strip()) == 0 or len(pes_sobrenome.strip()) == 0:
-        messages.add_message(request, constants.ERRO, 'Email ou nome não pode ser vazio')
+def validar_cadastro(request):  
+    pes_nome = request.POST['pes_nome']
+    pes_sobrenome = request.POST['pes_sobrenome']
+    pes_email = request.POST['pes_email']
+    pes_senha = request.POST['pes_senha']
+    emp_nome = request.POST['emp_nome']
+    
+    print(request.POST)
+    
+    if len(pes_nome.strip()) == 0 or len(pes_email.strip()) == 0 or len(pes_sobrenome.strip()) == 0 or len(emp_nome.strip()) == 0:
+        messages.add_message(request, constants.WARNING, 'Email, nome, Sobrenome ou Empresa não podem ser vazio')
         return redirect('/auth/cadastro')
     
-    if len(pes_senha) < 8:
-        messages.add_message(request, constants.ERRO, 'Sua senha deve ter no mínimo 8 caracteres')
+    elif len(pes_senha) < 8:
+        messages.add_message(request, constants.WARNING, 'Sua senha deve ter no mínimo 8 caracteres')
         return redirect('/auth/cadastro')
     
-    if User.objects.filter(pes_email = pes_email).exists():
-        messages.add_message(request, constants.ERRO, 'Já existe um usuário com esse e-mail')
+    elif User.objects.filter(email=pes_email).exists():
+        messages.add_message(request, constants.WARNING, 'Já existe um usuário com esse e-mail')
+        return redirect('/auth/cadastro')
+    
+    elif User.objects.filter(username=pes_nome).exists():
+        messages.add_message(request, constants.WARNING, 'Já existe um usuário ou empresa com esse nome')
         return redirect('/auth/cadastro')
     
     try:
-        usuario = User.objects.create_user(user_name = pes_nome, last_name = pes_sobrenome, email = pes_email, password = pes_senha)
+        usuario = User.objects.create_user(username=pes_nome, last_name=pes_sobrenome, email=pes_email, password=pes_senha)
         usuario.save()
+        print('Usuário salvo com sucesso!')
         return redirect('/auth/cadastro')
     
     except Exception as e:
